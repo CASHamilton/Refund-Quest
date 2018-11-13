ebef612d64129197fe2db5ac370147d10e56f4a9
Matt Dean <mattdean98@gmail.com>
2018-11-12 18:59:17 -0600

    Added Pause Menu

---
+++ Refund Quest/Assets/Scene Managment/pausemenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour {
+    public GameObject PauseCanvas;
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            PauseCanvas.SetActive(true);
+            //Cursor.visible = true;
+            //Cursor.lockState = CursorLockMode.None;
+
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PauseCanvas.SetActive(false);
+            //Cursor.visible = true;
+            //Cursor.lockState = CursorLockMode.None;
+
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
+        }
+    }
+    public void Resume()
+    {
+        //Debug.Log("Resume");
+        PauseCanvas.SetActive(false);
+        //Cursor.visible = false;
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
